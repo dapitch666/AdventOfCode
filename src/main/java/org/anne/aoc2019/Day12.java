4b7daa45f7aa2a45af9fbf970c04ff3f0931cd87
Dapitch666 <dapitch666@gmail.com>
2023-03-06 14:41:44 +0100

    2019 Day 12

---
+++ src/main/java/org/anne/aoc2019/Day12.java
@@ -0,0 +1,126 @@
+package org.anne.aoc2019;
+
+import org.anne.common.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day12  extends Day {
+
+    public static void main(String[] args) {
+        Day day = new Day12();
+        List<String> input = day.readFile();
+        day.setPart1(part1(input, 1000));
+        day.setPart2(part2(input));
+        day.printParts();
+    }
+
+    static int part1(List<String> input, int steps) {
+        List<Moon> moons = parseInput(input);
+        for (int i = 0; i < steps; i++) {
+            for (Moon moon : moons) {
+                for (Moon other : moons) {
+                    if (moon == other) continue;
+                    moon.applyGravity(other);
+                }
+            }
+            for (Moon moon : moons) {
+                moon.applyVelocity();
+            }
+        }
+        return moons.stream().map(Moon::getEnergy).reduce(0, Integer::sum);
+    }
+
+    static long part2(List<String> input) {
+        List<Moon> moons = parseInput(input);
+
+        long cycleX = 0;
+        long cycleY = 0;
+        long cycleZ = 0;
+        long steps = 0;
+
+        while (cycleX == 0 || cycleY == 0 || cycleZ == 0) {
+            for (Moon moon : moons) {
+                for (Moon other : moons) {
+                    if (moon == other) continue;
+                    moon.applyGravity(other);
+                }
+            }
+            for (Moon moon : moons) {
+                moon.applyVelocity();
+            }
+            steps++;
+            if (cycleX == 0 && moons.stream().allMatch(moon -> moon.velocity.x == 0)) {
+                cycleX = steps;
+            }
+            if (cycleY == 0 && moons.stream().allMatch(moon -> moon.velocity.y == 0)) {
+                cycleY = steps;
+            }
+            if (cycleZ == 0 && moons.stream().allMatch(moon -> moon.velocity.z == 0)) {
+                cycleZ = steps;
+            }
+        }
+        return lcm(lcm(cycleX, cycleY), cycleZ) * 2;
+    }
+
+    static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    static long gcd(long a, long b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+
+    static List<Moon> parseInput(List<String> input) {
+        List<Moon> moons = new ArrayList<>();
+        for (String line : input) {
+            String[] parts = line.substring(1, line.length() - 1).split(",");
+            int x = Integer.parseInt(parts[0].split("=")[1]);
+            int y = Integer.parseInt(parts[1].split("=")[1]);
+            int z = Integer.parseInt(parts[2].split("=")[1]);
+            moons.add(new Moon(new Point3d(x, y, z)) );
+        }
+        return moons;
+    }
+    protected record Point3d(int x, int y, int z) {
+        Point3d add(Point3d other) {
+            return new Point3d(x + other.x, y + other.y, z + other.z);
+        }
+        int absSum() {
+            return Math.abs(x) + Math.abs(y) + Math.abs(z);
+        }
+    }
+
+    static class Moon {
+        private Point3d position;
+        private Point3d velocity;
+
+        public Moon(Point3d position) {
+            this.position = position;
+            this.velocity = new Point3d(0, 0, 0);
+        }
+
+        public void applyGravity(Moon other) {
+            int x = Integer.compare(other.position.x, position.x);
+            int y = Integer.compare(other.position.y, position.y);
+            int z = Integer.compare(other.position.z, position.z);
+            velocity = velocity.add(new Point3d(x, y, z));
+        }
+
+        public void applyVelocity() {
+            position = position.add(velocity);
+        }
+
+        public int getEnergy() {
+            return position.absSum() * velocity.absSum();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return obj instanceof Moon other && other.position.equals(position) && other.velocity.equals(velocity);
+        }
+    }
+}
---
+++ src/test/java/org/anne/aoc2019/Day12Test.java
@@ -0,0 +1,27 @@
+package org.anne.aoc2019;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day12Test {
+
+    List<String> input = List.of(
+            "<x=-1, y=0, z=2>",
+            "<x=2, y=-10, z=-7>",
+            "<x=4, y=-8, z=8>",
+            "<x=3, y=5, z=-1>"
+    );
+
+    @Test
+    void testPart1() {
+        assertEquals(179, Day12.part1(input, 10));
+    }
+
+    @Test
+    void testPart2() {
+        assertEquals(2772, Day12.part2(input));
+    }
+}
