fa744da0466316e020bdd825327c359e4d0ebd99
Dapitch666 <dapitch666@gmail.com>
2023-12-02 16:52:12 +0100

    2023 Day 2

--- src/main/java/org/anne/aoc2023/Day2.java
+++ src/main/java/org/anne/aoc2023/Day2.java
@@ -17,10 +17,67 @@ public class Day2 extends Day {
 
 
     public static int part1(List<String> input) {
-        return 0;
+        int maxRed = 12, maxGreen = 13, maxBlue = 14;
+        int result = 0;
+        for (String line : input) {
+            String[] split = line.split(":");
+            String[] parts = split[1].split(";");
+            
+            int game = Integer.parseInt(split[0].split(" ")[1]);
+            boolean valid = true;
+            for (String part : parts) {
+                if (valid) {
+                    int red = 0, green = 0, blue = 0;
+                    String[] cubes = part.split(",");
+                    for (String cube : cubes) {
+                        String[] cubeParts = cube.trim().split(" ");
+                        int count = Integer.parseInt(cubeParts[0]);
+                        String color = cubeParts[1];
+                        switch (color) {
+                            case "red" -> red = count;
+                            case "green" -> green = count;
+                            case "blue" -> blue = count;
+                        }
+                        if (red > maxRed || green > maxGreen || blue > maxBlue) {
+                            valid = false;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (valid) {
+                result += game;
+            }
+        }
+        return result;
     }
 
-    public static int part2(List<String> input) {
-        return 0;
+    public static long part2(List<String> input) {
+        int result = 0;
+        for (String line : input) {
+            int maxRed = 0, maxGreen = 0, maxBlue = 0;
+            String[] split = line.split(":");
+            String[] parts = split[1].split(";");
+            for (String part : parts) {
+                int red = 0, green = 0, blue = 0;
+                String[] cubes = part.split(",");
+                for (String cube : cubes) {
+                    String[] cubeParts = cube.trim().split(" ");
+                    int count = Integer.parseInt(cubeParts[0]);
+                    String color = cubeParts[1];
+                    switch (color) {
+                        case "red" -> red = count;
+                        case "green" -> green = count;
+                        case "blue" -> blue = count;
+                    }
+                }
+                maxRed = Math.max(maxRed, red);
+                maxGreen = Math.max(maxGreen, green);
+                maxBlue = Math.max(maxBlue, blue);
+            }
+            
+            result += maxRed * maxGreen * maxBlue;
+        }
+        return result;
     }
 }
