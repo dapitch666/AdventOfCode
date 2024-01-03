1cae70dde77d6be2f42ae8f97202569b87963c16
dapitch666 <dapitch666@gmail.com>
2024-01-03 10:42:58 +0100

    2023 Day 25

--- src/main/java/org/anne/aoc2023/Day25.java
+++ src/main/java/org/anne/aoc2023/Day25.java
@@ -2,23 +2,88 @@ package org.anne.aoc2023;
 
 import org.anne.common.Day;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day25 extends Day {
     public static void main(String[] args) {
         Day day = new Day25();
         List<String> input = day.readFile();
         day.setPart1(part1(input));
-        day.setPart2(part2(input));
+        day.setPart2(part2());
         day.printParts();
     }
 
 
     public static int part1(List<String> input) {
-        return 0;
+        while (true) {
+            var graph = getGraph(input);
+            if (graph.reduce() == 3) {
+                 return graph.components().stream().map(component -> component.reduced.get()).reduce(1, (a, b) -> a * b);
+            }
+        }
     }
 
-    public static int part2(List<String> input) {
-        return 0;
+    public static String part2() {
+        return "Merry Christmas!";
+    }
+
+    static Graph getGraph(List<String> input) {
+        var graph = new Graph();
+        for (String line : input) {
+            String[] parts = line.split(": ");
+            var component = graph.getComponent(parts[0]);
+            String[] connected = parts[1].split(" ");
+            for (var connectedName : connected) {
+                component.addConnexion(graph.getComponent(connectedName));
+            }
+        }
+        return graph;
+    }
+
+    record Graph(List<Component> components, Map<String, Component> compMap) {
+
+        public Graph() {
+            this(new ArrayList<>(), new HashMap<>());
+        }
+
+        public Component getComponent(String name) {
+            var component = compMap.get(name);
+            if (component == null) {
+                component = compMap.computeIfAbsent(name, Component::new);
+                components.add(component);
+            }
+            return component;
+        }
+
+        public int reduce() {
+            while (components.size() > 2) {
+                var node1 = components.get(new Random().nextInt(components.size()));
+                var node2 = node1.connexions.get(new Random().nextInt(node1.connexions.size()));
+                node1.connexions.addAll(node2.connexions);
+                for (var node : node2.connexions) {
+                    node.connexions.remove(node2);
+                    node.connexions.add(node1);
+                }
+                node1.reduced.addAndGet(node2.reduced.get());
+                components.remove(node2);
+                node1.connexions.removeIf(n -> n == node1);
+            }
+            return components.get(0).connexions.size();
+        }
+    }
+
+    record Component(String name, List<Component> connexions, AtomicInteger reduced) {
+
+        public Component(String name) {
+            this(name, new ArrayList<>(), new AtomicInteger(1));
+        }
+
+        public void addConnexion(Component other) {
+            if (!connexions.contains(other)) {
+                connexions.add(other);
+                other.addConnexion(this);
+            }
+        }
     }
 }
--- src/test/java/org/anne/aoc2023/Day25Test.java
+++ src/test/java/org/anne/aoc2023/Day25Test.java
@@ -2,23 +2,35 @@ package org.anne.aoc2023;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day25Test {
 
     List<String> input = Arrays.asList(
+            "jqt: rhn xhk nvd",
+            "rsh: frs pzl lsr",
+            "xhk: hfx",
+            "cmg: qnr nvd lhk bvb",
+            "rhn: xhk bvb hfx",
+            "bvb: xhk hfx",
+            "pzl: lsr hfx nvd",
+            "qnr: nvd",
+            "ntq: jqt hfx bvb xhk",
+            "nvd: lhk",
+            "lsr: lhk",
+            "rzs: qnr cmg lsr rsh",
+            "frs: qnr lhk lsr"
     );
     
     @Test
     void testPart1() {
-        assertEquals(0, Day25.part1(input));
+        assertEquals(54, Day25.part1(input));
     }
 
     @Test
     void testPart2() {
-        assertEquals(0, Day25.part2(input));
+        assertEquals("Merry Christmas!", Day25.part2());
     }
 }
