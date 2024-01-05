8feabcc57a7274749cc31f47abe9092e246ff20b
dapitch666 <dapitch666@gmail.com>
2024-01-05 14:55:42 +0100

    2023 Day 22 part 2

--- src/main/java/org/anne/aoc2023/Day22.java
+++ src/main/java/org/anne/aoc2023/Day22.java
@@ -4,9 +4,9 @@ import org.anne.common.Day;
 import org.anne.common.Point3d;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
 import java.util.List;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day22 extends Day {
     public static void main(String[] args) {
@@ -17,99 +17,132 @@ public class Day22 extends Day {
         day.printParts();
     }
 
-
-    public static int part1(List<String> input) {
-        var bricks = input.stream().map(Brick::new).sorted().toList();
-        var support = new HashMap<Brick, List<Brick>>();
-        var isSupportedBy = new HashMap<Brick, List<Brick>>();
-        var occupied = new HashSet<Point3d>();
-        var disintegratable = new HashSet<Brick>();
-        for (var brick : bricks) {
-            brick.drop(occupied);
-            occupied.addAll(brick.getOccupied());
-            //System.out.println(brick + " " + brick.getOccupied());
+    public static long part1(List<String> input) {
+        var bricks = getBricks(input);
+        int[] countBricksDependencies = new int[bricks.size()];
+        for (var brick : bricks.stream().filter(b -> b.isSupportedBy.size() == 1).toList()) {
+            var i = bricks.indexOf(brick.isSupportedBy.stream().findFirst().orElseThrow());
+            countBricksDependencies[i]++;
         }
-        for (var brick : bricks) {
-            for (var other : bricks.stream().filter(b -> b.p1.z() == brick.p2.z() + 1).toList()) {
-                if (brick.supports(other)) {
-                    support.computeIfAbsent(brick, b -> new ArrayList<>()).add(other);
-                    isSupportedBy.computeIfAbsent(other, b -> new ArrayList<>()).add(brick);
-                }
-            }
-        }
-        //support.forEach((k, v) -> System.out.println(k.id + " supports " + v.stream().map(b -> b.id).toList()));
-        //isSupportedBy.forEach((k, v) -> System.out.println(k.id + " is supported by " + v.stream().map(b -> b.id).toList()));
-        for (var brick : bricks) {
-            if (!support.containsKey(brick)) {
-                disintegratable.add(brick);
-            } else if (support.get(brick).stream().allMatch(b -> isSupportedBy.get(b).size() > 1)) {
-                disintegratable.add(brick);
-            }
-        }
-        return disintegratable.size();
+        return Arrays.stream(countBricksDependencies).filter(i -> i == 0).count();
     }
 
     public static int part2(List<String> input) {
-        return 0;
+        var bricks = getBricks(input);
+        var result = 0;
+        for (Brick brick : bricks) {
+            var removed = new HashSet<Brick>();
+            removed.add(brick);
+            var supported = brick.supports;
+            while (!supported.isEmpty()) {
+                supported = supported.stream()
+                        .filter(supportedBrick -> removed.containsAll(supportedBrick.isSupportedBy))
+                        .peek(removed::add)
+                        .flatMap(supportedBrick -> supportedBrick.supports.stream())
+                        .collect(Collectors.toSet());
+            }
+            result += removed.size() - 1;
+        }
+        return result;
     }
-    
+
+    static List<Brick> getBricks(List<String> input) {
+        var bricks = input.stream().map(Brick::new).sorted().toList();
+        var occupied = new HashMap<Point3d, Integer>();
+
+        for (int i = 0; i < bricks.size(); i++) {
+            occupied.putAll(bricks.get(i).getOccupied(i));
+        }
+
+        for (int i = 0; i < bricks.size(); i++) {
+            var brick = bricks.get(i);
+            boolean supported = brick.start.z() == 1;
+            while(!supported) {
+                supported = brick.start.z() == 1;
+                if (brick.start.z() > 1) {
+                    var below = brick.pointsBelow().stream().filter(occupied::containsKey).toList();
+                    if (!below.isEmpty()) {
+                        supported = true;
+                        for (var point : below) {
+                            var brickBelow = bricks.get(occupied.get(point));
+                            brickBelow.supports.add(brick);
+                            brick.isSupportedBy.add(brickBelow);
+                        }
+                    }
+                    if (!supported) {
+                        occupied.keySet().removeAll(brick.getOccupied(i).keySet());
+                        brick.drop();
+                        occupied.putAll(brick.getOccupied(i));
+                    }
+                }
+            }
+        }
+        return bricks;
+    }
+
     static class Brick implements Comparable<Brick> {
-        Point3d p1;
-        Point3d p2;
-        String id;
-        
+        Point3d start;
+        int size;
+        int direction;
+        Set<Brick> supports = new HashSet<>();
+        Set<Brick> isSupportedBy = new HashSet<>();
+
+        final static int[] xs = new int[]{1, 0, 0};
+        final static int[] ys = new int[]{0, 1, 0};
+        final static int[] zs = new int[]{0, 0, 1};
+
         public Brick (String s) {
             String[] split = s.split("~");
             String[] p1 = split[0].split(",");
             String[] p2 = split[1].split(",");
-            this.p1 = new Point3d(Integer.parseInt(p1[0]), Integer.parseInt(p1[1]), Integer.parseInt(p1[2]));
-            this.p2 = new Point3d(Integer.parseInt(p2[0]), Integer.parseInt(p2[1]), Integer.parseInt(p2[2]));
-            //this.id = split[2];
-        }
-
-        public Brick(Point3d p1, Point3d p2) {
-            this.p1 = p1;
-            this.p2 = p2;
+            this.start = new Point3d(Integer.parseInt(p1[0]), Integer.parseInt(p1[1]), Integer.parseInt(p1[2]));
+            var end = new Point3d(Integer.parseInt(p2[0]), Integer.parseInt(p2[1]), Integer.parseInt(p2[2]));
+            if (this.start.x() != end.x()) {
+                direction = 0;
+                size = Math.abs(this.start.x() - end.x());
+            } else if (this.start.y() != end.y()) {
+                direction = 1;
+                size = Math.abs(this.start.y() - end.y());
+            } else if (this.start.z() != end.z()) {
+                direction = 2;
+                size = Math.abs(this.start.z() - end.z());
+            }
+            size++;
         }
 
         @Override
         public int compareTo(@NotNull Day22.Brick o) {
-            return Integer.compare(p1.z(), o.p1.z());
+            return Integer.compare(start.z(), o.start.z());
         }
-        
-        public boolean supports(Brick other) {
-            var newBrick = new Brick(other.p1.add(new Point3d(0, 0, -1)), other.p2.add(new Point3d(0, 0, -1)));
-            return newBrick.getOccupied().stream().anyMatch(p -> this.getOccupied().contains(p));
-        }
-        
-        public boolean isSupportedBy(Brick other) {
-            return other.supports(this);
-        }
-        
-        public List<Point3d> getOccupied() {
-            List<Point3d> occupied = new ArrayList<>();
-            for (int x = p1.x(); x <= p2.x(); x++) {
-                for (int y = p1.y(); y <= p2.y(); y++) {
-                    for (int z = p1.z(); z <= p2.z(); z++) {
-                        occupied.add(new Point3d(x, y, z));
-                    }
-                }
+
+        public Map<Point3d, Integer> getOccupied(int id) {
+            var occupied = new HashMap<Point3d, Integer>();
+            for (int i = 0; i < this.size; i++) {
+                var x = this.start.x() + i * xs[this.direction];
+                var y = this.start.y() + i * ys[this.direction];
+                var z = this.start.z() + i * zs[this.direction];
+                occupied.put(new Point3d(x, y, z), id);
             }
             return occupied;
         }
-        
-        public void drop(HashSet<Point3d> occupied) {
-            var newBrick = new Brick(p1.add(new Point3d(0, 0, -1)), p2.add(new Point3d(0, 0, -1)));
-            if (newBrick.p1.z() > 0 && occupied.stream().noneMatch(p -> newBrick.getOccupied().contains(p))) {
-                this.p1 = newBrick.p1;
-                this.p2 = newBrick.p2;
-                this.drop(occupied);
+
+        public List<Point3d> pointsBelow() {
+            if (this.direction == 2) {
+                return List.of(this.start.add(new Point3d(0, 0, -1)));
+            } else {
+                var points = new ArrayList<Point3d>();
+                for (int i = 0; i < this.size; i++) {
+                    var x = this.start.x() + i * xs[this.direction];
+                    var y = this.start.y() + i * ys[this.direction];
+                    var z = this.start.z() - 1;
+                    points.add(new Point3d(x, y, z));
+                }
+                return points;
             }
         }
 
-        @Override
-        public String toString() {
-            return this.id + "(" + this.p1.toString() + "~" + this.p2.toString() + ")";
+        public void drop() {
+            this.start = this.start.add(new Point3d(0, 0, -1));
         }
     }
 }
