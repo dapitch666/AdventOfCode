5b3e78d32f5b91e603270f593e9260f50ad403ec
dapitch666 <dapitch666@gmail.com>
2024-12-13 18:29:05 +0100

    2024 Day 13

--- src/main/java/org/anne/aoc2024/Day13.java
+++ src/main/java/org/anne/aoc2024/Day13.java
@@ -1,14 +1,11 @@
 package org.anne.aoc2024;
 
 import org.anne.common.Day;
-import org.anne.common.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Day13 extends Day {
     public static void main(String[] args) {
@@ -21,41 +18,38 @@ public class Day13 extends Day {
     }
 
     public static long part1(List<String> input) {
-        List<Machine> machines = getMachines(input, 0);
-        return machines.stream().mapToLong(Machine::minTokens).sum();
+        return getTotalTokens(input, 0);
     }
 
     public static long part2(List<String> input) {
-        List<Machine> machines = getMachines(input, 10000000000000L);
-        return machines.stream().mapToLong(Machine::minTokens).sum();
+        return getTotalTokens(input, 10000000000000L);
     }
 
-    private static List<Machine> getMachines(List<String> input, long offset) {
-        input.removeIf(String::isEmpty);
-        List<Machine> machines = new ArrayList<>();
+    private static long getTotalTokens(List<String> input, long offset) {
+        long tokens = 0;
         Pattern pattern = Pattern.compile("[XY][+=](\\d{1,5})");
-        // Create a list of all the machines
-        for (int i = 0; i < input.size(); i += 3) {
-            Matcher matcher = pattern.matcher(input.get(i) + input.get(i + 1) + input.get(i + 2));
-            List<Integer> parts = new ArrayList<>();
+        List<Long> parts = new ArrayList<>();
+        for (String line : input) {
+            Matcher matcher = pattern.matcher(line);
             while (matcher.find()) {
-                parts.add(Integer.parseInt(matcher.group(1)));
+                parts.add(Long.parseLong(matcher.group(1)));
             }
-            machines.add(new Machine(parts.get(0), parts.get(1), parts.get(2), parts.get(3),
-                    parts.get(4) + offset, parts.get(5) + offset));
         }
-        return machines;
+        for (int i = 0; i < parts.size(); i += 6) {
+            tokens += minTokens(parts.get(i), parts.get(i+1), parts.get(i+2), parts.get(i+3),
+                    parts.get(i+4) + offset, parts.get(i+5) + offset);
+        }
+        return tokens;
     }
 
-    record Machine(long aX, long aY, long bX, long bY, long pX, long pY) {
-        long minTokens() {
-            long numerator = pX * aY - pY * aX;
-            long b = numerator / (bX * aY - bY * aX);
-            long remX = pX - b * bX;
-            long l = aX == 0 ? pY : remX;
-            long r = aX == 0 ? aY : aX;
-            long a = l / r;
-            return (a * aY + b * bY == pY && l % r == 0) ? 3 * a + b : 0;
-        }
+    static long minTokens(long buttonAx, long buttonAy, long buttonBx, long buttonBy, long prizeX, long priceY) {
+        /* Finding the result is equivalent to solving the following system of equations:
+              buttonAx * aPress + buttonBx * bPress = prizeX
+              buttonAy * aPress + buttonBy * bPress = priceY
+         */
+        long bPress = (prizeX * buttonAy - priceY * buttonAx) / (buttonBx * buttonAy - buttonBy * buttonAx);
+        long aPress = (prizeX - bPress * buttonBx) / buttonAx;
+
+        return (aPress * buttonAx + bPress * buttonBx == prizeX && aPress * buttonAy + bPress * buttonBy == priceY) ? 3 * aPress + bPress : 0;
     }
 }
