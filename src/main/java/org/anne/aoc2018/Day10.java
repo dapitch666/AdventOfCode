ed5f60ec8b0f86dbcf34d372cacf5b75d01614cd
dapitch666 <dapitch666@gmail.com>
2025-01-04 16:27:28 +0100

    2018 Day 10

---
+++ src/main/java/org/anne/aoc2018/Day10.java
@@ -0,0 +1,63 @@
+package org.anne.aoc2018;
+
+import org.anne.common.Day;
+import org.anne.common.Pair;
+import org.anne.common.Utils;
+
+import java.awt.*;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Day10 extends Day {
+    public static void main(String[] args) {
+        new Day10().run();
+    }
+
+    @Override
+    public void execute() {
+        setName("The Stars Align");
+        List<String> input = readFile();
+        setPart1(part1(input, 10));
+        setPart2(part2(input, 10));
+        printParts();
+    }
+
+    public static String part1(List<String> input, int maxHeight) {
+        List<Pair<Point>> points = parseInput(input);
+        while (true) {
+            points.forEach(point -> point.first().translate(point.second().x, point.second().y));
+            int minY = points.stream().mapToInt(p -> p.first().y).min().orElseThrow();
+            int maxY = points.stream().mapToInt(p -> p.first().y).max().orElseThrow();
+            if (maxY - minY < maxHeight) {
+                return Utils.printAscii(points.stream().map(Pair::first).collect(Collectors.toSet()), " ", "#");
+            }
+        }
+    }
+
+    public static int part2(List<String> input, int maxHeight) {
+        List<Pair<Point>> points = parseInput(input);
+        int seconds = 0;
+        while (true) {
+            points.forEach(point -> point.first().translate(point.second().x, point.second().y));
+            seconds++;
+            int minY = points.stream().mapToInt(p -> p.first().y).min().orElseThrow();
+            int maxY = points.stream().mapToInt(p -> p.first().y).max().orElseThrow();
+            if (maxY - minY < maxHeight) {
+                return seconds;
+            }
+        }
+    }
+
+    private static List<Pair<Point>> parseInput(List<String> input) {
+        Pattern pattern = Pattern.compile("position=<\\s*(-?\\d+),\\s*(-?\\d+)> velocity=<\\s*(-?\\d+),\\s*(-?\\d+)>");
+        return input.stream()
+                .map(pattern::matcher)
+                .filter(Matcher::find)
+                .map(matcher -> new Pair<>(
+                        new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2))),
+                        new Point(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(4)))))
+                .collect(Collectors.toList());
+    }
+}
