9a9c760afde04a88908d798d0a3a66ee839dbb9e
dapitch666 <dapitch666@gmail.com>
2025-01-25 11:54:28 +0100

    2017 Day 14

--- src/main/java/org/anne/aoc2017/Day10.java
+++ src/main/java/org/anne/aoc2017/Day10.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static org.anne.aoc2017.KnotHash.knotHash;
+import static org.anne.aoc2017.KnotHash.sparseHash;
+
 public class Day10 extends Day {
     public static void main(String[] args) {
         new Day10().run();
@@ -24,41 +27,11 @@ public class Day10 extends Day {
     public static int part1(String input, int size) {
         List<Integer> lengths = Stream.of(input.split(",")).map(Integer::parseInt).toList();
         List<Integer> list = IntStream.range(0, size).boxed().collect(Collectors.toList());
-        knotHash(lengths, list, size, 1);
+        sparseHash(lengths, list, 1);
         return list.get(0) * list.get(1);
     }
 
     public static String part2(String input) {
-        List<Integer> lengths = input.chars().boxed().collect(Collectors.toList());
-        lengths.addAll(List.of(17, 31, 73, 47, 23));
-        List<Integer> list = IntStream.range(0, 256).boxed().collect(Collectors.toList());
-        knotHash(lengths, list, 256, 64);
-        StringBuilder hash = new StringBuilder();
-        for (int i = 0; i < 16; i++) {
-            int xor = list.get(i * 16);
-            for (int j = 1; j < 16; j++) {
-                xor ^= list.get(i * 16 + j);
-            }
-            hash.append(String.format("%02x", xor));
-        }
-        return hash.toString();
-    }
-
-    private static void knotHash(List<Integer> lengths, List<Integer> list, int size, int rounds) {
-        int currentPosition = 0;
-        int skipSize = 0;
-        for (int round = 0; round < rounds; round++) {
-            for (int length : lengths) {
-                for (int i = 0; i < length / 2; i++) {
-                    int index1 = (currentPosition + i) % size;
-                    int index2 = (currentPosition + length - i - 1) % size;
-                    int temp = list.get(index1);
-                    list.set(index1, list.get(index2));
-                    list.set(index2, temp);
-                }
-                currentPosition = (currentPosition + length + skipSize) % size;
-                skipSize++;
-            }
-        }
+        return knotHash(input);
     }
 }
---
+++ src/main/java/org/anne/aoc2017/Day14.java
@@ -0,0 +1,62 @@
+package org.anne.aoc2017;
+
+import org.anne.common.Day;
+
+import java.util.Arrays;
+
+import static org.anne.aoc2017.KnotHash.knotHash;
+import static org.anne.aoc2017.KnotHash.toBinaryString;
+
+public class Day14 extends Day {
+    public static void main(String[] args) {
+        new Day14().run();
+    }
+
+    @Override
+    public void execute() {
+        setName("Disk Defragmentation");
+        String input = readFileOneLine();
+        setPart1(part1(input));
+        setPart2(part2(input));
+        printParts();
+    }
+
+    public static int part1(String input) {
+        String[] grid = new String[128];
+        for (int i = 0; i < 128; i++) {
+            grid[i] = toBinaryString(knotHash(input + "-" + i));
+        }
+        return Arrays.stream(grid).mapToInt(s -> (int) s.chars().filter(c -> c == '1').count()).sum();
+    }
+
+    public static int part2(String input) {
+        Boolean[][] grid = new Boolean[128][128];
+        for (int y = 0; y < 128; y++) {
+            String hash = toBinaryString(knotHash(input + "-" + y));
+            for (int x = 0; x < 128; x++) {
+                    grid[y][x] = hash.charAt(x) == '1';
+            }
+        }
+        int regions = 0;
+        for (int y = 0; y < 128; y++) {
+            for (int x = 0; x < 128; x++) {
+                if (grid[y][x]) {
+                    regions++;
+                    markRegion(grid, x, y);
+                }
+            }
+        }
+        return regions;
+    }
+
+    private static void markRegion(Boolean[][] grid, int x, int y) {
+        if (x < 0 || x >= 128 || y < 0 || y >= 128 || !grid[y][x]) {
+            return;
+        }
+        grid[y][x] = false;
+        markRegion(grid, x - 1, y);
+        markRegion(grid, x + 1, y);
+        markRegion(grid, x, y - 1);
+        markRegion(grid, x, y + 1);
+    }
+}
