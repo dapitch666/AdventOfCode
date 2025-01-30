f4c7ddd45ccd203bb21176f7f717b70551f9cb33
dapitch666 <dapitch666@gmail.com>
2025-01-30 17:37:16 +0100

    2017 Day 25

--- src/main/java/org/anne/aoc2017/Day25.java
+++ src/main/java/org/anne/aoc2017/Day25.java
@@ -1,8 +1,11 @@
 package org.anne.aoc2017;
 
 import org.anne.common.Day;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.*;
+
+import static org.anne.common.Utils.last;
 
 public class Day25 extends Day {
     public static void main(String[] args) {
@@ -14,15 +17,53 @@ public class Day25 extends Day {
         setName("The Halting Problem");
         List<String> input = readFile();
         setPart1(part1(input));
-        setPart2(part2(input));
+        setPart2(part2());
         printParts();
     }
 
     public static int part1(List<String> input) {
-        return 0;
+        String currentState = input.get(0).split(" ")[3].substring(0, 1);
+        int steps = Integer.parseInt(input.get(1).replaceAll("\\D", ""));
+        List<State> states = getStates(input);
+
+        Map<Integer, Integer> tape = new HashMap<>();
+        int position = 0;
+
+        for (int i = 0; i < steps; i++) {
+            State state = states.get("ABCDEF".indexOf(currentState));
+            int currentValue = tape.getOrDefault(position, 0);
+            Rule action = state.rules.get(currentValue);
+            tape.put(position, action.write);
+            position += action.move;
+            currentState = action.nextState;
+        }
+        return (int) tape.values().stream().filter(value -> value == 1).count();
     }
 
-    public static int part2(List<String> input) {
-        return 0;
+    public static String part2() {
+        return "Merry Christmas!";
+    }
+
+    private static List<State> getStates(List<String> input) {
+        List<State> states = new ArrayList<>();
+
+        for (int i = 3; i < input.size(); i += 10) {
+            String name = input.get(i).split(" ")[2].substring(0, 1);
+            Map<Integer, Rule> actions = new HashMap<>();
+            for (int j = 0; j < 2; j++) {
+                int write = input.get(i + 2 + (j * 4)).contains("1") ? 1 : 0;
+                int move = input.get(i + 3 + j * 4).contains("right") ? 1 : -1;
+                String nextState = last(input.get(i + 4 + (j * 4)).split(" ")).substring(0, 1);
+                actions.put(j, new Rule(write, move, nextState));
+            }
+            states.add(new State(name, actions));
+        }
+        return states;
+    }
+
+    record State(String name, Map<Integer, Rule> rules) {
+    }
+
+    record Rule(int write, int move, String nextState) {
     }
 }
--- src/test/java/org/anne/aoc2017/Day25Test.java
+++ src/test/java/org/anne/aoc2017/Day25Test.java
@@ -9,15 +9,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day25Test {
 
-    List<String> input = Arrays.asList();
+    List<String> input = Arrays.asList(
+            "Begin in state A.",
+            "Perform a diagnostic checksum after 6 steps.",
+            "",
+            "In state A:",
+            "  If the current value is 0:",
+            "    - Write the value 1.",
+            "    - Move one slot to the right.",
+            "    - Continue with state B.",
+            "  If the current value is 1:",
+            "    - Write the value 0.",
+            "    - Move one slot to the left.",
+            "    - Continue with state B.",
+            "",
+            "In state B:",
+            "  If the current value is 0:",
+            "    - Write the value 1.",
+            "    - Move one slot to the left.",
+            "    - Continue with state A.",
+            "  If the current value is 1:",
+            "    - Write the value 1.",
+            "    - Move one slot to the right.",
+            "    - Continue with state A."
+    );
 
     @Test
     void testPart1() {
-        assertEquals(0, Day25.part1(input));
+        assertEquals(3, Day25.part1(input));
     }
 
     @Test
     void testPart2() {
-        assertEquals(0, Day25.part2(input));
+        assertEquals("Merry Christmas!", Day25.part2());
     }
 }
