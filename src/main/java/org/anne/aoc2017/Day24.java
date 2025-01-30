85384a9035d7af51db2bea293f4192fa20357161
dapitch666 <dapitch666@gmail.com>
2025-01-30 12:08:58 +0100

    2017 Day 24

--- src/main/java/org/anne/aoc2017/Day24.java
+++ src/main/java/org/anne/aoc2017/Day24.java
@@ -19,10 +19,50 @@ public class Day24 extends Day {
     }
 
     public static int part1(List<String> input) {
-        return 0;
+        List<Component> components = input.stream()
+                .map(line -> line.split("/"))
+                .map(parts -> new Component(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])))
+                .toList();
+        return bestBridge(components, 0, 0, 0, new State(0, 0), false).maxStrength;
     }
 
     public static int part2(List<String> input) {
-        return 0;
+        List<Component> components = input.stream()
+                .map(line -> line.split("/"))
+                .map(parts -> new Component(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])))
+                .toList();
+        return bestBridge(components, 0, 0, 0, new State(0, 0), true).maxStrength;
+    }
+
+    private static State bestBridge(List<Component> components, int currentPort, int length, int strength, State state, boolean part2) {
+        for (Component component : components) {
+            if (component.hasPort(currentPort)) {
+                component.used = true;
+                state = bestBridge(components, component.getOtherPort(currentPort), length + 1, strength + component.portA + component.portB, state, part2);
+                component.used = false;
+            }
+        }
+        return new State(!part2 || length == state.maxLength ? Math.max(strength, state.maxStrength) : state.maxStrength, Math.max(length, state.maxLength));
+    }
+
+    static class Component {
+        int portA, portB;
+        boolean used = false;
+
+        Component(int portA, int portB) {
+            this.portA = portA;
+            this.portB = portB;
+        }
+
+        boolean hasPort(int port) {
+            return !used & (portA == port || portB == port);
+        }
+
+        public int getOtherPort(int currentPort) {
+            return portA == currentPort ? portB : portA;
+        }
+    }
+
+    record State(int maxStrength, int maxLength) {
     }
 }
--- src/test/java/org/anne/aoc2017/Day24Test.java
+++ src/test/java/org/anne/aoc2017/Day24Test.java
@@ -9,15 +9,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day24Test {
 
-    List<String> input = Arrays.asList();
+    List<String> input = Arrays.asList(
+            "0/2",
+            "2/2",
+            "2/3",
+            "3/4",
+            "3/5",
+            "0/1",
+            "10/1",
+            "9/10"
+    );
 
     @Test
     void testPart1() {
-        assertEquals(0, Day24.part1(input));
+        assertEquals(31, Day24.part1(input));
     }
 
     @Test
     void testPart2() {
-        assertEquals(0, Day24.part2(input));
+        assertEquals(19, Day24.part2(input));
     }
 }
