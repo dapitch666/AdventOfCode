2317a532aca8ae1c778f7eee5b5a1ec93381806e
dapitch666 <dapitch666@gmail.com>
2025-02-07 13:43:04 +0100

    2016 Day 19

---
+++ src/main/java/org/anne/aoc2016/Day19.java
@@ -0,0 +1,40 @@
+package org.anne.aoc2016;
+
+import org.anne.common.Day;
+
+public class Day19 extends Day {
+    public static void main(String[] args) {
+        new Day19().run();
+    }
+
+    @Override
+    public void execute() {
+        setName("An Elephant Named Joseph");
+        int input = Integer.parseInt(readFileOneLine());
+        setPart1(part1(input));
+        setPart2(part2(input));
+        printParts();
+    }
+
+    public static int part1(int input) {
+        int elf = 1;
+        for (int i = 1; i <= input; i++) {
+            elf = (elf + 2 > i) ? 1 : elf + 2;
+        }
+        return elf;
+    }
+
+    public static int part2(int input) {
+        int elf = 1, prev = 1, increment = 1;
+        for (int i = 1; i <= input + 1; i++) {
+            increment = (elf >= prev) ? 2 : increment;
+            elf += increment;
+            if (elf >= i) {
+                prev = i - increment;
+                elf = 1;
+                increment = 1;
+            }
+        }
+        return elf;
+    }
+}
---
+++ src/test/java/org/anne/aoc2016/Day19Test.java
@@ -0,0 +1,19 @@
+package org.anne.aoc2016;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day19Test {
+
+
+    @Test
+    void testPart1() {
+        assertEquals(3, Day19.part1(5));
+    }
+
+    @Test
+    void testPart2() {
+        assertEquals(2, Day19.part2(5));
+    }
+}
